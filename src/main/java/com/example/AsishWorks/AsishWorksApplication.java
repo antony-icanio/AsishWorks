39e2ccd98e25ac3745f061a76e47006511aadca4
antony-icanio <antony.a@icanio.com>
2022-07-28 21:26:26 -0700

    first commit

---
+++ src/main/java/com/example/AsishWorks/AsishWorksApplication.java
@@ -0,0 +1,13 @@
+package com.example.AsishWorks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsishWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsishWorksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AsishWorks/AsishWorksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AsishWorks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsishWorksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
